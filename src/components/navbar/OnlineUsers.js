36d0442c8fb1ab98b728b9c5c7b8e44597e509c8
gokul <gokul465eswaran@gmail.com>
2022-05-08 14:12:48 +0530

    temp

--- src/components/navbar/OnlineUsers.js
+++ src/components/navbar/OnlineUsers.js
@@ -9,11 +9,10 @@ export default function OnlineUsers() {
   return (
     <div className="user-list">
       <h2>All Users</h2>
+      
       {error && <div className="error">{error}</div>}
       {documents &&
         documents.map((user) => (
-
-          
           <div key={user.id}>
             <span>{user.displayName}</span>
             <Avatar src={user.photoURL} />
